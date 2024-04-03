364506a4daef2be3e03fd1909566c1b1a32c8472
Ramón Huidobro <hola@ramonh.dev>
2024-04-03 14:39:53 +0200

    Integrate logo, make count dynamic

---
+++ src/components/DiscordBtn.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'preact/hooks';
+
+export default function DiscordBtn() {
+  const [memberCount, setMemberCount] = useState(0);
+  useEffect(() => {
+    const loadDiscordCount = async () => {
+      const response = await fetch(
+        'https://discord.com/api/v9/invites/SaWEM9vv?with_counts=true',
+      );
+      const data = await response.json();
+      setMemberCount(data.approximate_member_count);
+    };
+    loadDiscordCount();
+  }, []);
+
+  return (
+    <a href="https://discord.gg/jAbVkyGDRR" target="_blank" className="button">
+      Join our Discord! ({memberCount} members and counting)
+    </a>
+  );
+}
